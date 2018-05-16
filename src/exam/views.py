b1c96755c365635862a8b699e0c1a95d4880ced1
mohammad hasani <mohammad_hasani@live.com>
2018-05-16 19:46:16 +0430

    complete dichotic Single Section And Mirroing with Github

--- src/exam/views.py
+++ src/exam/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from . import exam_blureprint
-from .models import Exam, PureTone, Dichotic, Single,Binary,Ternary
+from .models import Exam, PureTone, Dichotic, Single, Binary, Ternary
 from flask_login import current_user, login_required
 from src.misc.constracts import EXAM_TYPE, EXAM_STATUS
 from mongoengine import errors as MongoEngineError
@@ -55,55 +55,37 @@ def pure_tone_report():
 @exam_blureprint.route('/dichotic', methods=['post'])
 @login_required
 def dichotic():
-
     req_json = request.get_json()
 
-    type = req_json['type']
-    exam_number = req_json['exam_number']
-    result = req_json['result']
-    new_dicho = Dichotic()
-    new_obj = eval(type.title())()
+    try:
+        dicho_type = req_json['type'].title()
+        exam_number = req_json['exam_number']
+        result = req_json['result']
+        new_dicho = Dichotic()
 
-    new_obj.create(exam_number, result)
-    new_dicho.create(type.title(), new_obj)
-    print(new_dicho[type.title()])
-    # new_pure_tone = PureTone(start_time=datetime.utcnow())
-    # new_pure_tone.create(property, result)
-    new_exam = Exam(user=current_user['id'], type=EXAM_TYPE['PURE_TONE'], dichotic=new_dicho)
-    new_exam.save()
+        new_obj = eval(dicho_type)()
 
-    # try:
-    #     type = req_json['type']
-    #     exam_number = req_json['exam_number']
-    #     result = req_json['result']
-    #     new_dicho = Dichotic()
-    #
-    #     new_obj = eval(type.title())()
-    #
-    #     passed_exam_before = Exam.passed_exam_before(current_user.id)
-    #
-    #     new_obj.create(exam_number, result)
-    #     new_dicho.create(type.title(), new_obj)
-    #     print(new_dicho[type.title()])
-    #     # new_pure_tone = PureTone(start_time=datetime.utcnow())
-    #     # new_pure_tone.create(property, result)
-    #     new_exam = Exam(user=current_user['id'], type=EXAM_TYPE['PURE_TONE'], pure_tone=new_dicho)
-    #     new_exam.save()
-    #
-    #     if passed_exam_before:
-    #         pure_tone = passed_exam_before.pure_tone
-    #         pure_tone[property] = result
-    #         passed_exam_before.save()
-    #     else:
-    #         new_obj.create(exam_number, result)
-    #         new_dicho.create(type.title(),new_obj)
-    #         print(new_dicho[type.title()])
-    #         # new_pure_tone = PureTone(start_time=datetime.utcnow())
-    #         # new_pure_tone.create(property, result)
-    #         new_exam = Exam(user=current_user['id'], type=EXAM_TYPE['PURE_TONE'], pure_tone=new_dicho)
-    #         new_exam.save()
-    #
-    # except:
-    #     pass
+        passed_exam_before = Exam.passed_exam_before(current_user.id)
+
+        if passed_exam_before:
+            dicho_type_exam = getattr(passed_exam_before.dichotic, dicho_type)
+            overwrite_dicho_type = Dichotic.overwrite(dicho_type_exam, result)
+            setattr(passed_exam_before.dichotic,dicho_type,overwrite_dicho_type)
+
+            # setattr(passed_exam_before, 'dichotic', passed_exam_before.dichotic)
+            print(passed_exam_before.dichotic.Single.d4)
+            print(type(passed_exam_before))
+            passed_exam_before.save()
+            # print(type(passed_exam_before))
+
+        else:
+            new_obj.create(exam_number, result)
+            new_dicho.create(dicho_type, new_obj)
+
+            new_exam = Exam(user=current_user['id'], type=EXAM_TYPE['PURE_TONE'], dichotic=new_dicho)
+            new_exam.save()
+
+    except Exception as e:
+        print(e)
 
     return 'ok'
